2942f6e152152961b48296ed983c6aabeb8ff700
robinHurtado <robin_aven94@hotmail.com>
2018-05-03 21:52:46 -0500

    Add histoy api and Add id in user_type.js

--- client/index.js
+++ client/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client'; // interacts with the backend
+import { ApolloProvider } from 'react-apollo'; // glue layer between apollo and react
+import { Router, Route, IndexRoute } from 'react-router';
+import  createBrowserHistory  from 'history/createBrowserHistory';
+
+import App from './components/App';
+
+const history = createBrowserHistory();
+console.log(history)
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
-    <div>
-      Auth Starter
-    </div>
+    <ApolloProvider client={client}>
+      <Router history={history}>
+        <Route path="/" component={App}>
+        </Route>
+      </Router>
+    </ApolloProvider>
   );
 };
 
